9c7c902269f007184ec28d651c3d4e1e9e456ec5
Roope Hakulinen <roope.hakulinen@gmail.com>
2017-07-28 20:55:07 +0300

    Fix the problems with AoT build not finding all symbols.

--- src/ngx-line-chart.ts
+++ src/ngx-line-chart.ts
@@ -1,2 +1,4 @@
 // Public classes.
 export { NgxLineChartModule } from './ngx-line-chart.module';
+export { LineChartComponent } from './line-chart/line-chart.component';
+export { ChartComponent } from './chart/chart.component';
