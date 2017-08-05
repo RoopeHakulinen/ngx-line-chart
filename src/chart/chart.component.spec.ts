56d5f30e9e0c2dd9f0eab8c9ca6d765d7bae0760
Roope Hakulinen <roope.hakulinen@gmail.com>
2017-08-06 00:45:11 +0300

    Make the ChartComponent basic rendering test succeed.

--- src/chart/chart.component.spec.ts
+++ src/chart/chart.component.spec.ts
@@ -1,6 +1,28 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ChartComponent } from './chart.component';
+import { defaultStyle } from '../default-style';
+
+const testSet1 = {
+  name: 'test set 1',
+  points: [
+    { x: 0, y: 100 },
+    { x: 1, y: 200 },
+    { x: 2, y: 300 },
+  ]
+};
+const testSet2 = {
+  name: 'test set 2',
+  points: [
+    { x: 0, y: 100 },
+    { x: 4, y: 200 },
+    { x: 8, y: 300 },
+  ]
+};
+const testAxis1 = {
+  min: 0,
+  max: 500
+};
 
 describe('ChartComponent', () => {
   let component: ChartComponent;
@@ -16,6 +38,8 @@ describe('ChartComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ChartComponent);
     component = fixture.componentInstance;
+    component.dataSets = [testSet1, testSet2];
+    component.style = defaultStyle;
     fixture.detectChanges();
   });
 
